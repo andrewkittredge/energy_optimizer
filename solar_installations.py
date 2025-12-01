a6038c4f2506946a2e4d69ea4aed2d57f13c1a91
Andrew Kittredge <andrewlkittredge@gmail.com>
2025-12-01 17:39:37 +0000

    fix order

---
+++ solar_installations.py
@@ -0,0 +1,11 @@
+INSTALLATION_SIZES = {
+    3: 9300,
+    5: 15000,
+    6: 18000,
+    8: 12600,
+    8: 16800,
+    10: 20000,
+    12: 24000,
+}
+"""
+Maps solar installation sizes (in kW) to their corresponding costs (in USD)."""
