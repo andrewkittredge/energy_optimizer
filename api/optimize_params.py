042f0c74b055ec995ae070408724b40d05618000
Andrew Kittredge <andrewlkittredge@gmail.com>
2025-12-29 21:22:22 +0000

    make keys a float

--- api/optimize_params.py
+++ api/optimize_params.py
@@ -9,12 +9,12 @@ class OptimizeParams(BaseModel):
     peak_consumption: float = 10
     off_peak_consumption: float = 20
     # Accept JSON string keys (typical when sent from JS). We'll coerce to int below.
-    solar_installation_sizes: Dict[str, float] = {
-        "3": 0.282,
-        "5": 0.250,
-        "6": 0.230,
-        "8": 0.210,
-        "10": 0.190,
-        "12": 0.117,
+    solar_installation_sizes: Dict[float, float] = {
+        3: 0.282,
+        5: 0.250,
+        6: 0.230,
+        8: 0.210,
+        10: 0.190,
+        12: 0.117,
     }
 
