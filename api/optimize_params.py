48a68fd25d0678222ce0d77a6fcf11b29304bc2f
Andrew Kittredge <andrewlkittredge@gmail.com>
2025-12-29 18:18:59 +0000

    set the defaults

--- api/optimize_params.py
+++ api/optimize_params.py
@@ -3,11 +3,18 @@ from pydantic import BaseModel
 
 
 class OptimizeParams(BaseModel):
-    peak_price: Optional[float] = None
-    off_peak_price: Optional[float] = None
-    battery_cost_per_kw: Optional[float] = None
-    peak_consumption: Optional[float] = None
-    off_peak_consumption: Optional[float] = None
+    peak_price: float = 0.5
+    off_peak_price: float = 0.4
+    battery_cost_per_kw: float = 0.15
+    peak_consumption: float = 10
+    off_peak_consumption: float = 20
     # Accept JSON string keys (typical when sent from JS). We'll coerce to int below.
-    solar_installation_sizes: Optional[Dict[str, float]] = None
+    solar_installation_sizes: Dict[str, float] = {
+        "3": 0.282,
+        "5": 0.250,
+        "6": 0.230,
+        "8": 0.210,
+        "10": 0.190,
+        "12": 0.117,
+    }
 
