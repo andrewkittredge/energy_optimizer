a5e2de4c2145fc3d5c90523feeb38b6f54f837ab
Andrew Kittredge <andrewlkittredge@gmail.com>
2026-01-08 21:57:33 -0500

    fix url

--- api/scratch.py
+++ api/scratch.py
@@ -1,13 +1,13 @@
 import asyncio
 from fastmcp import Client
 
-client = Client("http://localhost:8000/mcp")
+client = Client("http://127.0.0.1:8000/solar/mcp")
 
 
 async def call_tool(name: str):
     async with client:
         tools = await client.list_tools()
-        x = await client.call_tool("get_defaults_defaults_get")
+        print("Available tools:", tools)
         result = await client.call_tool("greet", {"name": name})
         print(result)
 
